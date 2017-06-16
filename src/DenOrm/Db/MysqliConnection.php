6be75b48b342a846e1474efb08748a2eb9ab999c
Deniz Ozsen <deniz.ozsen@gmail.com>
2017-06-16 13:35:01 +0100

    MysqliConnection: fix query() by converting named parameters to question mark placeholders

--- src/DenOrm/Db/MysqliConnection.php
+++ src/DenOrm/Db/MysqliConnection.php
@@ -16,15 +16,19 @@ class MysqliConnection extends Connection
      */
     public function query($sql, $parameters = [])
     {
-        $statement = $this->mysqli->prepare($sql);
-        foreach ($parameters as $name => $value) {
+        // Convert PDO-style named parameters to question mark placeholders,
+        // because mysqli does not support named parameters
+        $this->convertNamedParameters($sql, $parameters);
+
+        $statement = $this->mysqli->prepare($mysqli_sql);
+        foreach ($mysqli_parameters as $i => $value) {
             $type = 's';
             if (is_integer($value)) {
                 $type = 'i';
             } elseif (is_double($value)) {
                 $type = 'd';
             }
-            $statement->bind_param($type, $parameters[$name]);
+            $statement->bind_param($type, $parameters[$i]);
         }
         $result = $statement->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
@@ -45,4 +49,27 @@ class MysqliConnection extends Connection
     {
         return $this->mysqli->real_escape_string($string);
     }
+
+    /**
+     * Converts the PDO-style named parametrers in the given SQL and parameter array
+     * to the question mark placeholders, accepted by mysqli prepared statements. Both
+     * the given SQL string and paramerter array arguments are replaced by reference.
+     * Note that the $parameters array must be associative, but will be replaced with
+     * a simple indexed array of values.
+     *
+     * @param string $sql SQL string, passed by reference
+     * @param array $parameters associative array of named parameters, passed by reference
+     */
+    public function convertNamedParameters(&$sql, &$parameters)
+    {
+        $converted_parameters = [];
+        $callback = function(array $matches) use($parameters, &$converted_parameters) {
+            $converted_parameters[] = $parameters[$matches[0]];
+            return '?';
+        };
+
+        $sql = preg_replace_callback('/:[^\s]+/', $callback, $sql);
+        $parameters = $converted_parameters;
+    }
 }
+

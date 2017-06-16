41c457eb7538c2b3a1b0aecbeb7fa6457f9d2ef6
Deniz Ozsen <deniz.ozsen@gmail.com>
2017-06-16 13:36:52 +0100

    Start of unit test for MysqliConnection

---
+++ tests/DenOrm/Db/MysqliConnectionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DenOrm\Db;
+
+class MysqliConnectionTest extends \PHPUnit_Framework_TestCase
+{
+    private $mysqli_connection;
+
+    public function setUp()
+    {
+        $mock_mysqli = $this->createMock('mysqli');
+        $this->mysqli_connection = new MysqliConnection($mock_mysqli);
+    }
+
+    /**
+     * @test
+     */
+    public function first()
+    {
+        $sql = "SELECT * FROM my_table WHERE my_a = :my_a AND my_b = :something_else";
+        $parameters = [ ':my_a' => 'value_for_a', ':something_else' => 'another_value' ];
+
+        $expected_sql_result = "SELECT * FROM my_table WHERE my_a = ? AND my_b = ?";
+        $expected_parameters_result = [ 'value_for_a', 'another_value' ];
+
+        $this->mysqli_connection->convertNamedParameters($sql, $parameters);
+        $this->assertEquals($expected_sql_result, $sql);
+        $this->assertEquals($expected_parameters_result, $parameters);
+    }
+}
+?>
